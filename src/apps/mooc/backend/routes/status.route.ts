d0ea7d2b6112412a409b4e488648330d93ce0dee
Joseph Morales <josephmss2799@gmail.com>
2024-10-20 18:43:16 +0100

    add node dependency injection personal doc

--- src/apps/mooc/backend/routes/status.route.ts
+++ src/apps/mooc/backend/routes/status.route.ts
@@ -4,8 +4,12 @@ import StatusController from '../controllers/StatusGetController';
 import container from '../dependency-injection';
 
 export const register = (router: Router): void => {
-	const controller = container.get<StatusController>('Apps.mooc.controllers.StatusGetController');
-	router.get('/status', (req: Request, res: Response) => {
-		controller.run(req, res);
-	});
+  /**
+   * extraemos la dependencia del .yml , especificamos
+   * el tipo y lo extraemos con el namespace
+   */
+  const controller = container.get<StatusController>('Apps.mooc.controllers.StatusGetController');
+  router.get('/status', (req: Request, res: Response) => {
+    controller.run(req, res);
+  });
 };
