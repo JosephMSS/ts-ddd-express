99f6b8f9bf016c7b48cce4e7318b9a0959110703
Joseph Morales <josephmss2799@gmail.com>
2024-10-20 18:33:35 +0100

    add personal documentation

---
+++ docs.md
@@ -0,0 +1,4 @@
+## Health check
+
+- Unicamente valida el estado del servidor, por que?
+  El propósito es solo validar que mi servidor esta caído sino tengo que levantar otro, de lo contrario estaría levantando servidores en un bucle infinito y no me daría cuenta que es la base de datos que no funciona.
--- src/apps/mooc/backend/routes/index.ts
+++ src/apps/mooc/backend/routes/index.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express';
 import glob from 'glob';
-
+/**
+ * *Escanea todos los ficheros con la extensión .route. y los carga en el
+ * *router.register que se espera que tenga ese archivo
+ * @param router
+ */
 export function registerRoutes(router: Router): void {
-	const routes = glob.sync(`${__dirname}/**/*.route.*`);
-	routes.map(route => register(route, router));
+  const routes = glob.sync(`${__dirname}/**/*.route.*`);//no son expresiones regulares pero es para especificar el path, en este caso  ** significa que sea a cualquier nivel
+  routes.map(route => register(route, router));
 }
 
 function register(routePath: string, router: Router) {
-	// eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
-	const { register } = require(routePath) as { register: (router: Router) => void };
-	register(router);
+  // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
+  const { register } = require(routePath) as { register: (router: Router) => void };
+  register(router);
 }
