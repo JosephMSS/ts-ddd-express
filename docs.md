ba9ce902019ff08806c3b79d9ed59fdd0d0b5b78
Joseph Morales <josephmss2799@gmail.com>
2024-10-21 09:06:54 -0600

    add personal docs

--- docs.md
+++ docs.md
@@ -17,3 +17,6 @@ estos no deben ser presentados al usuario, por que?
 
 -los datos de del est de aceptación debe cumplir los términos visibles para el cliente, en el casos de course create este test solo response a un estatus 200 pero no nos verifica si el recurso se creo correctamente, por lo que se puede agregar un test para verificar que el recurso se creo correctamente mas adelante en la recuperación
 para est tenemos diferente tipo e test por lo que nos podemos apoyar en el test unitario .
+La interfaz es sin sufijo o prefijo para poder adaptarla a las classes agregamos el por que, por ejemplo repository puede ser mongoRepository, postgresRepository, mysqlRepository, etc.
+
+en casos de testing. a veces en los repos u otras clases debemos agregar clases para testear por ejemplo el dominio no ocupa el search pero nuestro repo si lo necesita para ver que se guardaron los datos
--- tests/Contexts/Mooc/Courses/infrastructure/persistance/FileCourseRepository.test.ts
+++ tests/Contexts/Mooc/Courses/infrastructure/persistance/FileCourseRepository.test.ts
@@ -1,17 +1,17 @@
-import { Course } from "../../../../../../src/Contexts/Mooc/Courses/domain/Course";
-import { FileCourseRepository } from "../../../../../../src/Contexts/Mooc/Courses/infrastructure/persistence/FileCourseRepository";
+import { Course } from '../../../../../../src/Contexts/Mooc/Courses/domain/Course';
+import { FileCourseRepository } from '../../../../../../src/Contexts/Mooc/Courses/infrastructure/persistence/FileCourseRepository';
 
-describe("FileCourseRepository", () => {
-  it("should be save a course", async () => {
-    const expectedCourse = new Course({
-      id: "1",
-      name: "Course 1",
-      duration: "100",
-    });
-    const repository = new FileCourseRepository();
-    await repository.save(expectedCourse);
+describe('FileCourseRepository', () => {
+	it('should be save a course', async () => {
+		const expectedCourse = new Course({
+			id: '1',
+			name: 'Course 1',
+			duration: '100'
+		});
+		const repository = new FileCourseRepository();
+		await repository.save(expectedCourse);
 
-    const course =await repository.search(expectedCourse.id);
-    expect(course).toEqual(expectedCourse);
-  });
+		const course = await repository.search(expectedCourse.id);
+		expect(course).toEqual(expectedCourse);
+	});
 });
