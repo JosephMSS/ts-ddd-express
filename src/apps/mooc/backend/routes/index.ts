158c84f6100ed74c3f3056f02c59500c18f68ec8
Joseph Morales <josephmss2799@gmail.com>
2024-10-21 10:06:41 -0600

    validation protocol

--- src/apps/mooc/backend/routes/index.ts
+++ src/apps/mooc/backend/routes/index.ts
@@ -1,17 +1,35 @@
-import { Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
+import { ValidationError, validationResult } from 'express-validator';
 import glob from 'glob';
+import httpStatus from 'http-status';
 /**
  * *Escanea todos los ficheros con la extensión .route. y los carga en el
  * *router.register que se espera que tenga ese archivo
  * @param router
  */
 export function registerRoutes(router: Router): void {
-  const routes = glob.sync(`${__dirname}/**/*.route.*`);//no son expresiones regulares pero es para especificar el path, en este caso  ** significa que sea a cualquier nivel
-  routes.map(route => register(route, router));
+	const routes = glob.sync(`${__dirname}/**/*.route.*`); //no son expresiones regulares pero es para especificar el path, en este caso  ** significa que sea a cualquier nivel
+	routes.map(route => register(route, router));
 }
 
-function register(routePath: string, router: Router) {
-  // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
-  const { register } = require(routePath) as { register: (router: Router) => void };
-  register(router);
+function register(routePath: string, router: Router): void {
+	// eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
+	const { register } = require(routePath) as { register: (router: Router) => void };
+	register(router);
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export function validateReqSchema(req: Request, res: Response, next: NextFunction) {
+	const validationErrors = validationResult(req);
+	if (validationErrors.isEmpty()) {
+		next();
+
+		return;
+	}
+	const errors = validationErrors.array().map((err: ValidationError) => {
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+		return { [err.param]: err.msg };
+	});
+
+	return res.status(httpStatus.UNPROCESSABLE_ENTITY).json(errors);
 }
--- tests/apps/mooc/backend/features/courses/create-course.feature
+++ tests/apps/mooc/backend/features/courses/create-course.feature
@@ -7,9 +7,20 @@ Feature: Create a new course
     Given I send a PUT request to "/courses/ef8ac118-8d7f-49cc-abec-78e0d05af80a" with body:
     """
     {
+      "id":"d92cf24e-b3e4-4885-9592-5d6996a6733c",
       "name": "The best course",
       "duration": "5 hours"
     }
     """
     Then the response status code should be 201
     And the response should be empty
+  Scenario: An invalid non existing course
+    Given I send a PUT request to "/courses/ef8ac118-8d7f-49cc-abec-78e0d05af80a" with body:
+    """
+    {
+      "id":"d92cf24e-b3e4-4885-9592-5d6996a6733c",
+      "name": 5,
+      "duration": "5 hours"
+    }
+    """
+    Then the response status code should be 422
